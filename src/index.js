0ddb5f97ac710578663cc88fe2b01a9b75c72f11
Shambhavi Maya <shambhavimaya@gmail.com>
2026-09-16 00:52:18 +0530

    fix: remove duplicate add/greet declarations in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-function add(a, b) {
-  return a + b;
-}
-
-function greet(name) {
-  if (!name) throw new Error("name is required");
-  return `Hello, ${name}!`;
-}
-
-module.exports = { add, greet };
-
 const http = require("http");
 
 function add(a, b) {
